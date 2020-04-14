77b72dc211f245297df6912072b79802e87cb1c2
Mary <mary@codechrysalis.io>
2020-04-15 04:03:44 +0900

    Testing public tracking

---
+++ public/precache-manifest.213d87a947530239a7d1c3a2feecbafc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    revision: "6fb6c389203a8a02eb0305569134ecab",
+    url: "/index.html",
+  },
+  {
+    revision: "13fecaa3d86940b73dd9",
+    url: "/static/css/main.27fd15f6.chunk.css",
+  },
+  {
+    revision: "7657f0ada994a1d1d18c",
+    url: "/static/js/2.441dc7ed.chunk.js",
+  },
+  {
+    revision: "f0d43bb23094b147d2c500466b241ef2",
+    url: "/static/js/2.441dc7ed.chunk.js.LICENSE.txt",
+  },
+  {
+    revision: "13fecaa3d86940b73dd9",
+    url: "/static/js/main.aca49335.chunk.js",
+  },
+  {
+    revision: "6f0234b4b87d585d06e8",
+    url: "/static/js/runtime-main.cfae3202.js",
+  },
+  {
+    revision: "ab10d000cdf7ee45a57395feee160ad2",
+    url: "/static/media/logo.ab10d000.png",
+  },
+]);
